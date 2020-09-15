fc64d18017052441a8b6b7c09065e0febb712f67
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2020-09-15 20:04:06 -0300

    Commit de comentarios

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
+// Esta é a div em que o jogo vai acontecer
 const jogo = document.getElementById("jogo");
 
+// Esta é a referência que o jogo usará para criar a área de quadros. A área total será o quadrado do valor abaixo
+const referenciaBaseAltura = 15;
+
+// Quando uma tecla for pressionada para mudar a direção da cobra, a função mudaDirecao será disparada
 const body = document.getElementsByTagName("body")[0];
 body.addEventListener("keydown", (tecla) => mudaDirecao(tecla.key));
 
-const referenciaBaseAltura = 15;
+// Esta é o tempo que cobra leva para se locomover. O nível de dificuldade aumenta se o valor diminui
 let tempoMovimento = 1000;
 
+// Este sera o vetor que guardará os quadros que aparecerão na tela
 let area = [];
 
+// Aqui o loop monta o objeto de quadros
 for (let x = 0; x < referenciaBaseAltura; x++) {
     area.push([]);
     for (let y = 0; y < referenciaBaseAltura; y++) {
@@ -15,11 +22,13 @@ for (let x = 0; x < referenciaBaseAltura; x++) {
     }
 }
 
+// A cobra é representada logicamente por este objeto
 let snake = {
     direcao: "left",
     coordenadas: {idxLinha: 5, idxColuna: 8}
 }
 
+// A cobra se locomove com base nesta lógica
 let gameRunning = setInterval(() => {
     switch (snake.direcao) {
         case "up":
@@ -51,10 +60,12 @@ let gameRunning = setInterval(() => {
     })
 }, tempoMovimento);
 
+// Caso o game termine
 function gameOver() {
     clearInterval(gameRunning);
 }
 
+// Esta função muda a direção da cobra
 function mudaDirecao(direcao) {
 
     const condicionais = [
